e16f5ac40ff5825f1cd30cdefe0d38f6ac546259
Yash Patel <pyash1691999@gmail.com>
2021-02-15 10:49:34 +0530

    Pagination Done

--- src/app/main/product/product.component.ts
+++ src/app/main/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductModel } from 'src/app/interfaces/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { PagerService } from 'src/app/services/pager.service';
 import { ProductService } from 'src/app/services/product.service';
 import { ToasterService } from 'src/app/services/toaster.service';
 
@@ -16,11 +17,14 @@ export class ProductComponent implements OnInit {
   public products: ProductModel[];
   public totalProducts: number;
   public searchTitle: string;
+  public pager: any = {};
+  public pagedItems: ProductModel[];
 
   constructor(private router: Router,
               private productService: ProductService, 
               private cartService : CartService,
               private toasterService: ToasterService,
+              private pagerService: PagerService,
               ) {}
 
   ngOnInit(): void {
@@ -29,21 +33,30 @@ export class ProductComponent implements OnInit {
       .subscribe(
         (products: ProductModel[]) => {
           this.isLoading = false;
+          console.log(products);
           this.products = products;
           this.productService.productsList = products;
+          this.setPage(1);
         }
       );
+    
   }
 
+  public setPage(page: number) {
+    this.pager = this.pagerService.getPager(this.products.length, page);
+    this.pagedItems = this.products.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
   public onEdit(id: number): void {
     this.router.navigateByUrl(`/product/edit/${id}`);
   }
 
   public onDelete(id: number): void {
     this.productService.deleteProduct(id).subscribe(
-      () => {
+      (resdata) => {
+        console.log(resdata);
         this.toasterService.showSuccess('Product Deleted Successfully',this.products[id].title);
         this.products.splice(id, 1);
+        this.setPage(this.pager.currentPage);
       },
       (error) => {
         this.toasterService.showError('Error', error);
@@ -58,6 +71,7 @@ export class ProductComponent implements OnInit {
           this.products[index].title,
           'Added to the cart'
         );
+        this.setPage(this.pager.currentPage);
       },
       (error) => {
         this.toasterService.showError('Error', error);
